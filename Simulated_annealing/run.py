b62a5fc1e3bcbb4ad53e287defda15b7f71eb95b
EM <eleonora.misino@gmail.com>
2020-03-31 10:51:14 +0200

    Add parser

--- Simulated_annealing/run.py
+++ Simulated_annealing/run.py
@@ -1,6 +1,7 @@
 
 #-----------Set the environment---------------
 import sys, os
+import argparse
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,13 +14,33 @@ from test_functions import ackley_fn, rastrigin_fn, rosenbrock_fn, himmelblau_fn
 #rnd.seed(42)
 
 if __name__ == '__main__':
+
+    #---------------Parser----------------
+
+    parser = argparse.ArgumentParser(description='Simulated Annealing Algorithm', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-t', '--init_temp', action='store', nargs='?', const=None, default=100, type=float,
+                        choices=None, help='Initial temperature. [default: 100]', metavar=None)
+    parser.add_argument('-k', '--k_max', action='store', nargs='?', const=None, default=1e6, type=int,
+                        choices=None, help='Max number of iterations. [default: 1e6]', metavar=None)
+    parser.add_argument('-tv', '--tolerance_value', action='store', nargs='?', const=None, default=1e-6, type=int,
+                        choices=None, help='Tolerance Energy value for stopping criterion [default: 1e-6]', metavar=None)
+    parser.add_argument('-ti', '--tolerance_iter', action='store', nargs='?', const=None, default=1000, type=int,
+                        choices=None, help='Number of iterations taken into account in Tolerance Energy. [default: 1000]', metavar=None)
+    parser.add_argument('-o', '--obj_fn_limit', action='store', nargs='?', const=None, default=-1e10, type=float,
+                        choices=None, help='Objective function limit. [default: -1e10]', metavar=None)
+    parser.add_argument('-r', '--reann', action='store', nargs='?', const=None, default=100, type=float,
+                        choices=None, help='Reanniling tolerance value.[default: 100]', metavar=None)
+    parser.add_argument('-v', '--verbose', action='store', nargs='?', const=None, default=False, type=bool,
+                        choices=None, help='[default: False]', metavar=None)
+    args = parser.parse_args()
     
     #--------Set the test configuration---------
+
     test_conf = {
-        "Ackley" : [ackley_fn, (-6, 6), 100. ],
-        "Himmelblau" : [himmelblau_fn, (-6, 6), 100. ],
-        "Rastrigin" : [rastrigin_fn, (-5.12, 5.12), 100. ],
-        "Rosenbrock" : [rosenbrock_fn, (-6, 6), 100. ]
+        "Ackley" : [ackley_fn, (-6, 6) ],
+        "Himmelblau" : [himmelblau_fn, (-6, 6)],
+        "Rastrigin" : [rastrigin_fn, (-5.12, 5.12)],
+        "Rosenbrock" : [rosenbrock_fn, (-6, 6)]
     }
     results = {
         "Ackley" : [],
@@ -42,13 +63,13 @@ if __name__ == '__main__':
                                                         energy = par[0],
                                                         move = boltz_move,
                                                         interval = par[1],
-                                                        initial_temp = par[2],
-                                                        k_max = 10000,
-                                                        tolerance_value = 1e-10,
-                                                        tolerance_iter = 1000,
-                                                        obj_fn_limit = 1e-10,
-                                                        reann_tol = 100,
-                                                        verbose = True
+                                                        initial_temp = args.init_temp,
+                                                        k_max = args.k_max,
+                                                        tolerance_value = args.tolerance_value,
+                                                        tolerance_iter = args.tolerance_iter,
+                                                        obj_fn_limit = args.obj_fn_limit,
+                                                        reann_tol = args.reann,
+                                                        verbose = args.verbose
                                                         )
         results[fn].append(states)
         results[fn].append(energies)
